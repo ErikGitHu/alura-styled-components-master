e2117ee8f9288633159ec7f625eae8f37332c0ff
Erik Barbosa Miranda <erikbarbosamiranda@hotmail.com>
2023-04-18 19:57:24 -0300

    Updates

---
+++ precache-manifest.1835fa11a12de7934f218aea54485d4b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d24d58fb437d29bc7d34338c73f51419",
+    "url": "/alura-styled-components-master/index.html"
+  },
+  {
+    "revision": "8a5f440905592d4f96c7",
+    "url": "/alura-styled-components-master/static/css/main.397c896b.chunk.css"
+  },
+  {
+    "revision": "375a9e757d712271f63f",
+    "url": "/alura-styled-components-master/static/js/2.b6cb0ab3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/alura-styled-components-master/static/js/2.b6cb0ab3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a5f440905592d4f96c7",
+    "url": "/alura-styled-components-master/static/js/main.b1d6afde.chunk.js"
+  },
+  {
+    "revision": "82e23dc7d5ad1de761a4",
+    "url": "/alura-styled-components-master/static/js/runtime-main.cc4cd678.js"
+  },
+  {
+    "revision": "f3a2bff49642ca68b3841fde422295cb",
+    "url": "/alura-styled-components-master/static/media/bank_logo.f3a2bff4.svg"
+  },
+  {
+    "revision": "b97c300bed11302a8687c16e07aa89e5",
+    "url": "/alura-styled-components-master/static/media/dinheiro.b97c300b.svg"
+  },
+  {
+    "revision": "acd41ceb3366a71c61b7ed3d5df83682",
+    "url": "/alura-styled-components-master/static/media/olho.acd41ceb.svg"
+  },
+  {
+    "revision": "134d853a95b2b30676da2c63cda2eeaf",
+    "url": "/alura-styled-components-master/static/media/privado.134d853a.svg"
+  }
+]);
