1137833b308359abe432571eb76619d74366ef03
Erik Barbosa Miranda <erikbarbosamiranda@hotmail.com>
2023-04-18 15:05:52 -0300

    first commit

---
+++ src/Components/Cabecalho/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import bank_logo from "../../assets/images/bank_logo.svg";
+
+const Cabecalho = () => {
+  return (
+    <div className="cabecalho">
+      <img className="imagem-logo" src={bank_logo} alt="Logo Smart Bank" />
+      <div>
+        <a className="btn-secundario" href="https://www.google.com.br">
+          Sair
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Cabecalho;
